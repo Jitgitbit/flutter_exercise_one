eddad5a2cd0ccb8437854466c2072e3da131f222
Jitgitbit <rowowolf@gmail.com>
2020-05-11 15:16:50 +0200

    Connecting Functions & Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    void answerQuestion(){
+      print('Answer chosen!');
+    }
+
     var questions = ['What\'s your favorite color?', 'Do you like trains?'];
 
     return MaterialApp(home: Scaffold(
@@ -17,9 +21,9 @@ class MyApp extends StatelessWidget {
       ),
       body: Column(children: [
         Text('The question!'),
-        RaisedButton(child: Text('Answer 1'), onPressed: null,),
-        RaisedButton(child: Text('Answer 2'), onPressed: null,),
-        RaisedButton(child: Text('Answer 3'), onPressed: null,),
+        RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
+        RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion,),
+        RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion,),
       ],),
     ),);
     
