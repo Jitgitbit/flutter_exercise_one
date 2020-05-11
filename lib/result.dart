3e428841c425fb2b16561322b8a6067c0919f145
Jitgitbit <rowowolf@gmail.com>
2020-05-11 22:52:41 +0200

    Getters & "else-if"

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase{
+    String resultText;
+    if(resultScore <= 5){
+      resultText = 'You are awesome!';
+    }else if(resultScore <= 7){
+      resultText = 'Pretty likeable';
+    }else if(resultScore <= 10){
+      resultText = 'Not so great!';
+    }else{
+      resultText = 'That is awful!';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
